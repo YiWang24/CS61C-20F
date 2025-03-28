d3181317da1a769f0fbaae5e6291e589b15151f9
YiWang24 <yiwang2457@gmail.com>
2025-03-28 15:08:37 -0400

    done lab1

---
+++ lab/lab01/ll_cycle.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "ll_cycle.h"
+
+int ll_has_cycle(node *head) {
+    node *tortoise = head;
+	node *hare = head;
+	while (hare != NULL && hare->next != NULL && hare->next->next != NULL) {
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if (tortoise == hare) {
+			return 1;
+		}
+	}
+	return 0;
+}
